1bfd5c99b12c45b98a8946d27ec33e0ad941a0c2
Yasmim Castro <67880069+0302cmd@users.noreply.github.com>
2024-05-23 20:27:15 -0300

    main

---
+++ html-css-js-portfolio-tutorial-2-main/script.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
