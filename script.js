5d271f359b99343baeeb6fd732b8b57b6a7987e8
Yasmim Castro <67880069+0302cmd@users.noreply.github.com>
2024-05-31 13:18:13 -0300

    adicionando botao para voltar ao topo

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function toggleMenu() {
 function openInNewTab(url) {
   window.open(url, "_blank").focus();
 }
+
+// Seleciona o botão
+const backToTopButton = document.getElementById("back-to-top");
+
+// Adiciona um listener de rolagem na janela
+window.addEventListener("scroll", () => {
+  // Verifica a quantidade de rolagem vertical
+  if (window.scrollY > 300) {
+    backToTopButton.style.display = "flex";
+    backToTopButton.style.opacity = "1";
+  } else {
+    backToTopButton.style.display = "none";
+    backToTopButton.style.opacity = "0";
+  }
+});
+
+// Adiciona um listener de clique ao botão
+backToTopButton.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth", // Rola suavemente para o topo
+  });
+});
